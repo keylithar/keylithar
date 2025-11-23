aa5573c51a20061399cb968ed1e601f941402e7e
keylithar <contacto.keylithar@gmail.com>
2025-11-23 20:05:46 +0100

    Update blackfriday.js

--- blackfriday.js
+++ blackfriday.js
@@ -4,7 +4,7 @@
    true  = ACTIVAR Black Friday (todos los precios a 26 €)
    false = Precios normales
    ========================================================== */
-const MODO_OFERTAS = true; // ← CAMBIA AQUÍ A true o false
+const MODO_OFERTAS = false; // ← CAMBIA AQUÍ A true o false
 const PRECIO_BLACKFRIDAY = "26 €";
 
 // Asegurarse que el DOM está cargado
@@ -37,3 +37,4 @@ false → Precios normales
 
 
 Guarda el archivo y recarga la página. ✅ */
+
